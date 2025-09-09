6f8f48fe97a3b1f21863a56f4fde62b001accebc
Jacob Colon <jj.colon8@gmail.com>
2025-09-09 14:55:43 -0400

    Created background

--- SwiftCalculator/SwiftCalculator/ContentView.swift
+++ SwiftCalculator/SwiftCalculator/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var displayText: String = "100"
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            ZStack {
+                RadialGradient(
+                    gradient: Gradient(colors: [.white, .blue.opacity(0.8)]),
+                    center: .leading,
+                    startRadius: 10,
+                    endRadius: 400
+                )
+                RadialGradient(
+                    gradient: Gradient(colors: [.blue.opacity(0.2),.white.opacity(0.8)]),
+                    center: .trailing,
+                    startRadius: 10,
+                    endRadius: 500
+                )
+            }.ignoresSafeArea()
+            
+            GeometryReader { proxy in
+                VStack {
+                    VStack {
+                        Text(displayText)
+                            .font(.system(size: 50, weight: .bold))
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    .frame(
+                        maxWidth: .infinity,
+                        maxHeight: proxy.size.height * 0.25
+                        )
+                }
+                .ignoresSafeArea()
+                .padding()
+            }
+            
+            
         }
-        .padding()
     }
 }
 
