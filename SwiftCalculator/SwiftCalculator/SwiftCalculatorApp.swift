8428e631b1db93b0a15f455e897023f9ab78138c
Jacob Colon <jj.colon8@gmail.com>
2025-09-09 14:00:48 -0400

    first commit

---
+++ SwiftCalculator/SwiftCalculator/SwiftCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCalculatorApp.swift
+//  SwiftCalculator
+//
+//  Created by Jacob Colon on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
